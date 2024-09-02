00b1089a0064a25f64a89d8d126258e0073fb5ce
Srijan Samridh <srijan.samridh@coindcx.com>
2024-09-02 18:21:55 +0530

    test

--- lib/prefect_class.dart
+++ lib/prefect_class.dart
@@ -13,7 +13,7 @@ abstract class NotificationService {
 class DatabaseUserRepository implements UserRepository {
   @override
   void saveUser(User user) {
-    debugPrint('Saving user ${user.userName} to the database.');
+    debugPrint('Saving user ${user.userName} to the database and check.');
     // Implement actual database save logic
   }
 }
